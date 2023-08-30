f9d2fa9f7c49cdd484722a784b5f63e37c30018e
erdemgazeloglu <erdemgazeloglu@gmail.com>
2023-08-30 11:31:20 +0300

    Classlar düzenlendi

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const modes = [["Major", "Ionian"], "Dorian", "Phrygian", "Lydian", "Mixolydian", ["Natural Minor", "Aeolian"], "Locrian"];
 const isSharpedScales = ["B", "E", "A", "D", "G", "C"];
 
+const cMajor = ["C", "D", "E", "F", "G", "A", "B"];
+const fMajor = ["F", "G", "A", "Bb", "C", "D", "E"];
+
 const table = document.getElementById("table");
 const buttons = document.getElementsByTagName("button");
 
@@ -18,14 +21,15 @@ for (button of buttons) {
   });
 }
 
-// diyezli gamlar
-const sharpedScales = {
-  C: ["C", "D", "E", "F", "G", "A", "B"],
+class Scale {
+  constructor(root, scale) {
+    this[root] = scale; 
+  }
 
   // yalnızca dizileri döndürür.
   getScales() {
     return (Object.keys(this).filter((item) => Array.isArray(this[item])));
-  },
+  }
 
   // kullanıcının seçtiği notayı objenin içerisinde ara
   search(userInputNote) {
@@ -34,30 +38,33 @@ const sharpedScales = {
         return (this.hasOwnProperty(userInputNote)) ? this[userInputNote] : this.createScale(userInputNote);
       } 
     }
-  },
-
-  // kök sesi "note" olan diyezli gam olana kadar objeye gam ekler
-  createScale(note) {
-    // Yalnızca array niteliklerini içeren diziyi alın
-    let scales = this.getScales();
-    // İşlemi her zaman en son gam üzerinden yapıyoruz.
-    let lastScale = scales[scales.length - 1];
-    // gamın bir shallow copy'sini aldık
-    let newScale = [...this[lastScale]];
-    // ilk dört elemanı çıkartıp sonuna ekledik.
-    let extracted = newScale.splice(0, 4);
-    newScale = newScale.concat(extracted); 
-    // son elemana diyez attık
-    newScale[newScale.length - 1] += "#";
-    this[newScale[0]] = newScale;
-
-    this.search(note);
   }
-
-};
-
-flattedScales() {
-  F: ["F", "G", "A", "Bb", "C", "D", "E"],
-
-  
 }
+
+// diyezli gamlar
+const sharpedScales = new Scale(cMajor[0], cMajor);
+
+// bemollü gamlar
+const flattedScales = new Scale(fMajor[0], fMajor);
+
+console.log(sharpedScales);
+console.log(flattedScales);
+
+// kök sesi "note" olan diyezli gam olana kadar objeye gam ekler
+sharpedScales.createScale = function(note) {
+  // Yalnızca array niteliklerini içeren diziyi alın
+  let scales = this.getScales();
+  // İşlemi her zaman en son gam üzerinden yapıyoruz.
+  let lastScale = scales[scales.length - 1];
+  // gamın bir shallow copy'sini aldık
+  let newScale = [...this[lastScale]];
+  // ilk dört elemanı çıkartıp sonuna ekledik.
+  let extracted = newScale.splice(0, 4);
+  newScale = newScale.concat(extracted); 
+  // son elemana diyez attık
+  newScale[newScale.length - 1] += "#";
+  this[newScale[0]] = newScale;
+
+  this.search(note);
+}
+
